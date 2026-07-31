9efcd5499e14017d0b4d6ad3d28a93cb6a8094f1
masa <kakigawatei@gmail.com>
2026-07-31 14:05:47 +0900

    印刷の罫線を月末まで四角に閉じる(月末後を空マスで埋め+行を等分)・線を黒に・注意書きを大きく上部余白へ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION="eigyocal-v10";
+var CACHE_VERSION="eigyocal-v11";
 var SHELL=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE_VERSION).then(function(c){return c.addAll(SHELL);}).catch(function(){}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE_VERSION;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
